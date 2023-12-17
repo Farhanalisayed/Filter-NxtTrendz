ea2a7767260a390912cee1bc178d7f5386c87961
Farhanalisayed <151043508+Farhanalisayed@users.noreply.github.com>
2023-12-17 15:16:01 +0530

    Update index.js

--- src/components/FiltersGroup/index.js
+++ src/components/FiltersGroup/index.js
@@ -10,29 +10,69 @@ const FiltersGroup = props => {
     onResetting,
     categoryOptions,
     ratingsList,
+    activeRatingID,
+    activeCategoryID,
+    searchInput,
   } = props
 
-  let searchInput
   const onInputChanged = event => {
-    if (event.target.value !== '' && event.key === 'Enter') {
+    if (event.key === 'Enter') {
       updateSearchInput(event.target.value)
-      searchInput = event.target.value
     }
-    searchInput = ''
   }
 
   const onClicked = () => {
-    const inputVal = document.getElementById('input').value
+    let input = document.getElementById('input').value
     updateSearchInput(inputVal)
   }
 
-  const onCategoryChanged = event => {
-    updateCategory(event.target.each.id)
-  }
+  const renderCategories = () => (
+    <div className="category-cont">
+      <h1 className="category-header">Category</h1>
+      <ul className="category-cont">
+        {categoryOptions.map(each => {
+          const onCategoryChanged = () => {
+            updateCategory(each.categoryId)
+          }
+          const isActive = activeCategoryID === each.id
+          const catClass = isActive ? 'clicked-category' : 'category'
 
-  const onRatingChanged = event => {
-    updateRatings(event.target.each.id)
-  }
+          return (
+            <li onClick={onCategoryChanged}>
+              <button className={catClass} type="button">
+                <p>{each.name}</p>
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+
+  const rendeRatings = () => (
+    <div>
+      <h1 className="rating-heading">Rating</h1>
+      <ul className="ratings-cont">
+        {ratingsList.map(each => {
+          const onRatingChanged = () => {
+            updateRatings(each.ratingId)
+          }
+          const ratingClass = activeRatingID === each.id ? 'clicked-up' : 'up'
+
+          return (
+            <li className="stars-cont" onClick={onRatingChanged}>
+              <img
+                className="star"
+                src={each.imageUrl}
+                alt={`rating ${each.ratingId}`}
+              />
+              <p className={ratingClass}>& up</p>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 
   const onReset = () => {
     onResetting()
@@ -47,37 +87,13 @@ const FiltersGroup = props => {
           id="input"
           placeholder="Search"
           onKeyDown={onInputChanged}
-          value={searchInput}
           className="bar"
         />
         <IoIosSearch className="icon" onClick={onClicked} />
       </div>
 
-      <div className="category-cont">
-        <h1 className="category-header" value={0} onClick={onCategoryChanged}>
-          Category
-        </h1>
-        <ul className="category-cont">
-          {categoryOptions.map(each => (
-            <li className="category" onClick={onCategoryChanged}>
-              {each.name}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <ul className="ratings-cont">
-        {ratingsList.map(each => (
-          <li onClick={onRatingChanged} className="stars-cont">
-            <img
-              className="star"
-              src={each.imageUrl}
-              alt={`rating${each.ratingId}`}
-            />
-            <p className="up">& up</p>
-          </li>
-        ))}
-      </ul>
+      {renderCategories()}
+      {rendeRatings()}
 
       <button onClick={onReset} className="btn">
         Clear Filters
